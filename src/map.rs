d8b68ee6e583edfeb08d7cdaa23684a96048909a
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-03 21:57:55 -0700

    (feat) Scaffolded collections traits for Map.

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use std::collections;
 
 /// A key value store, implemented as a persistent, functional
 /// size balanced binary search tree.
@@ -23,5 +24,42 @@ pub enum Map<K, V> {
 
     /// A leaf node.
     Tip
+}
+
+impl<K, V> collections::Collection for Map<K, V> {
+    #[inline]
+    fn len(&self) -> uint {
+        match *self {
+            Bin { size, .. } => size,
+            Tip => 0
+        }
+    }
+}
+
+impl<K: Ord + Send + Share, V: Send + Share> collections::Map<K, V> for Map<K, V> {
+    fn find<'a>(&'a self, lookup: &K) -> Option<&'a V> {
+        match *self {
+            Bin { ref key, ref left, ref right, ref value, .. } => match key.deref().cmp(lookup) {
+                Equal   => Some(&**value),
+                Less    => left.find(lookup),
+                Greater => right.find(lookup)
+            },
+            Tip => None
+        }
+    }
+}
+
+impl<K: Ord + Send + Share, V: Send + Share> collections::Set<K> for Map<K, V> {
+    fn contains(&self, lookup: &K) -> bool {
+        self.find(lookup).is_some()
+    }
+
+    fn is_disjoint(&self, _other: &Map<K, V>) -> bool {
+        unimplemented!()
+    }
+
+    fn is_subset(&self, _other: &Map<K, V>) -> bool {
+        unimplemented!()
+    }
 }
 
